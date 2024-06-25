1f63b642ad136362e677dd0b413f73f4ff7677a7
Mohamed Sabry <abosaadmohamed@gmail.com>
2024-06-26 00:56:09 +0700

    solve the heart icon issue

--- src/components/Home/ContentTop.tsx
+++ src/components/Home/ContentTop.tsx
@@ -4,7 +4,7 @@ function ContentTop() {
   return (
     <div className=" flex flex-col items-center justify-center w-full border-b border-white">
       <div className="border border-white rounded-full w-16 h-16 sm:w-20 sm:h-20 flex items-center justify-center ">
-        <span className="text-white text-3xl sm:text-4xl">&#10084;</span>
+        <span className="text-white text-4xl sm:text-5xl ">&hearts;</span>
       </div>
       <div className=" w-[1px] h-12 bg-white" />
     </div>
